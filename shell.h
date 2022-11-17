2f0c1d917fda526e47739e494ef94e7d2c541e44
valjacques Nyemb <nyvanyemb@gmail.com>
2022-11-17 01:28:47 +0100

    add shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+char **split_string(char *string, char **divided_string);
+void execute_program(char *buffer, char *argv[]);
+
+#endif
