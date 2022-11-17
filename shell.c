b7a8cd51edff3d043ceca01c9037b0ff92227d9e
nyemb <nyvanyemb@gmail.com>
2022-11-16 17:40:06 -0800

    add new shell

--- shell.c
+++ shell.c
@@ -1,95 +1,118 @@
 #include "shell.h"
 
 
+/**
+ * main - Simple shell enter point
+ * @argc: Argument count
+ * @argv: arguments list
+ * Return: Exit value by status
+ */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
-  char *buffer, *check_exit = "exit";
-  size_t buffer_size = 0;
-  pid_t pid;
+	char *buffer, *check_exit = "exit";
+	size_t buffer_size = 0;
+	pid_t pid;
 
-  buffer = malloc(sizeof(char) * buffer_size);
-  if (buffer == NULL)
-    exit(1);
-  printf("$ ");
-  /*Use getline to accept commmands*/
-  while (getline(&buffer, &buffer_size, stdin) != -1)
-  {
-    buffer[strcspn(buffer, "\n")] = 0;
-    if (strcmp(buffer, check_exit) == 0)
-    {
-      free(buffer);
-      return (0);
-    }
-    pid = fork();
-    if (pid == -1)
-    {
-      free(buffer);
-      return (-1);
-    }
-    if (pid == 0)
-    {
-      execute_program(buffer, argv);
-    }
-    else
-    {
-      wait(NULL);
-      free(buffer);
-      buffer = malloc(sizeof(char) * buffer_size);
-      if (buffer == NULL)
-        exit(1);
-      printf("$ ");
-    }
-  }
-  free(buffer);
-  return (0);
+	buffer = malloc(sizeof(char) * buffer_size);
+	if (buffer == NULL)
+		exit(1);
+	printf("$ ");
+	/*Use getline to accept commmands*/
+	while (getline(&buffer, &buffer_size, stdin) != -1)
+	{
+		buffer[strcspn(buffer, "\n")] = 0;
+		if (strcmp(buffer, check_exit) == 0)
+		{
+			free(buffer);
+			return (0);
+		}
+		pid = fork();
+		if (pid == -1)
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (pid == 0)
+		{
+			execute_program(buffer, argv);
+		}
+		else
+		{
+			wait(NULL);
+			free(buffer);
+			buffer = malloc(sizeof(char) * buffer_size);
+			if (buffer == NULL)
+				exit(1);
+			printf("$ ");
+		}
+	}
+	free(buffer);
+	return (0);
 }
+
+
+/**
+ * execute_program - Execute simple shell command
+ * @buffer: Input command
+ * @argv: list of args
+ */
 
 void execute_program(char *buffer, char *argv[])
 {
-  int i, number_of_words = 1, value;
-  char **argvec, **divided_string;
+	int i, number_of_words = 1, value;
+	char **argvec, **divided_string;
 
-  /*Use strtok to split the buffer with space as separator*/
-  for (i = 0; i < (int)strlen(buffer); i++)
-  {
-    if (buffer[i] == ' ')
-    number_of_words++;
-  }
-  divided_string = malloc(sizeof(char *) * number_of_words);
-  if (divided_string == NULL)
-  exit(1);
-  /*Put the split string inside argv array*/
-  argvec = split_string(buffer, divided_string);
-  /*Run execve on the command*/
-  value = execvp(argvec[0], argvec);
-  if (value == -1)
-  {
-    free(divided_string);
-    perror(argv[0]);
-    printf("$ ");
-  }
-  free(buffer);
+	/*Use strtok to split the buffer with space as separator*/
+	for (i = 0; i < (int)strlen(buffer); i++)
+	{
+		if (buffer[i] == ' ')
+			number_of_words++;
+	}
+	divided_string = malloc(sizeof(char *) * number_of_words);
+	if (divided_string == NULL)
+		exit(1);
+	/*Put the split string inside argv array*/
+	argvec = split_string(buffer, divided_string);
+	/*Run execve on the command*/
+	value = execvp(argvec[0], argvec);
+	if (value == -1)
+	{
+		free(divided_string);
+		perror(argv[0]);
+		printf("$ ");
+	}
+	free(buffer);
 }
+
+
+/**
+ * split_strings - split line of Input
+ * @string: line of cmd to Parse
+ * @divided_string: split
+ * Return:  Array of char
+ */
 
 char **split_string(char *string, char **divided_string)
 {
-  /*create divided_string variable*/
-  char *splits;
-  char *sep;
-  int i = 0;
+	/*create divided_string variable*/
+	char *splits;
+	char *sep;
+	int i = 0;
 
-  sep = " ";
-  /*use strtok to get first token*/
-  splits = strtok(string, sep);
-  /*while token isn't equal to NULL*/
-  while (splits != NULL)
-  {
-    /*assign divided string array to token*/
-    divided_string[i] = splits;
-    /*strtok(NULL)*/
-    splits = strtok(NULL, sep);
-    i++;
-  }
-  /*return array*/
-  return (divided_string);
+	sep = " ";
+	/*use strtok to get first token*/
+	splits = strtok(string, sep);
+	/*while token isn't equal to NULL*/
+	while (splits != NULL)
+	{
+		/*assign divided string array to token*/
+		divided_string[i] = splits;
+		/*strtok(NULL)*/
+		splits = strtok(NULL, sep);
+		i++;
+	}
+	/*return array*/
+	return (divided_string);
 }
+
