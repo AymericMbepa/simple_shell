21f984cb49ed75286d20ab6aebca24135700f89c
AymericMbepa <aymericmbepa@gmail.com>
2022-11-18 05:59:18 -0800

    task1

--- shell.c
+++ shell.c
@@ -1,45 +1,153 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#define _GNU_SOURCE
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-/**
- * main - entry point
- *
- * Return: always 0 succeed
- */
+#define LSH_BUFSIZE 1024
+#define LSH_TOK_DELIM " \t\r\n\a"
 
-int main(void)
+char *lsh_read_line()
 {
-	extern char **environ;
-	char **av = malloc(2 * sizeof(char *));
-	pid_t pid;
-	int statut;
-	char *line = NULL;
-	size_t size = 0;
+	int bufsize = LSH_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char)*bufsize);
+	int c;
 
+	if (!buffer)
+	{
+		fprintf(stderr, "lsh allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 	while (1)
 	{
-		pid = fork();
-		if (pid == 0)
+		c = getchar();
+		if (c == EOF || c == '\n')
 		{
-			printf("#cisfun$ ");
-			if (getline(&line, &size, stdin) < 0)
-			{
-				fprintf(stderr, "fail to get command line");
-				exit(1);
-			}
-			av[0] = line;
-			av[1] = NULL;
-			printf("%s\n", av[0]);
-			if (execve(av[0], av, environ) == -1)
-				perror("./shell");
-			free(av);
+			buffer[position] = '\0';
+			return buffer;
 		}
 		else
-			wait(&statut);
+		{
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= bufsize)
+		{
+			bufsize += LSH_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer)
+			{
+				fprintf(stderr, "lsh allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
-	return (0);
+}
+
+char **lsh_spilt_line(char *line)
+{
+	/*int bufsize = LSH_BUFSIZE;
+	position = 0;*/
+	char **tokens = malloc(2 * sizeof(char*));
+	/*char *token;*/
+
+	if (!tokens)
+	{
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	tokens[0] = line;
+	tokens[1] = NULL;
+	/*token = strtok(line, LSH_TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position ++;
+
+		if (position >= LSH_BUFSIZE)
+		{
+			bufsize += LSH_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "lsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, LSH_TOK_DELIM);
+	}
+	tokens[position] = NULL;*/
+	return tokens;
+}
+
+int lsh_launch(char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		/* Child process*/
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("./shell");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+	{
+		/* Error forking*/
+		perror("./shell");
+	}
+	else
+	{
+		/* Parent process*/
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return 1;
+}
+
+int lsh_execute(char **args)
+{
+
+	if (args[0] == NULL)
+	{
+		/* An empty command was entered.*/
+		return 1;
+	}
+	return lsh_launch(args);
+}
+
+void lsh_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		printf("#cisfun$ ");
+		line = lsh_read_line();
+		args = lsh_spilt_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while(status);
+}
+
+/**
+ * main - execute programme
+ */
+int main(__attribute__((unused))int argc, __attribute__((unused))
+	 char const *argv[])
+{
+	lsh_loop();
+	return EXIT_SUCCESS;
 }
