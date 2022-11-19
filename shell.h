ab018e2738a16ed6357ae0a63fab1d935fa986b2
valjacques Nyemb <nyvanyemb@gmail.com>
2022-11-19 03:31:23 +0100

    change executing path

--- shell.h
+++ shell.h
@@ -1,53 +1,112 @@
-#ifndef SHELL_BASIC_H
-#define SHELL_BASIC_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
+/**###### LIBS USED ######*/
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <linux/limits.h>
 
-/*  File prototypes  */
-void shell_loop(char **args);
-char *shell_read_line(void);
-char **shell_split_line(char *line);
-int shell_launch(char **args, int flag);
-int shell_execute(char **args);
-void _free_double_pointer(char **d_pointer);
-char *_getline_command(void);
 
-/* Functions for Built-in commands */
-int shell_cd(char **args);
-int shell_help(char **args);
-int shell_exit(char **args);
-int shell_env(char **environ);
-int lsh_eof(char **args);
-int shell_num_builtins(void);
-/* Environment variables and Function´s */
-char *_getenv(const char *name);
-extern char **environ;
-void handle_signal(int m);
-/* getfile */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getfile(char **lineptr, size_t *n, FILE *stream);
-/* paths  */
-char *test_path(char **path, char *command);
-char *append_path(char *path, char *command);
-char *find_path(void);
-/* Functions for strings */
-int _strlen(char *string);
-int _strcmp(char *s1, char *s2);
-int _strncmp(char *s1, char *s2, int n);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, const char *src);
-char *str_concat(char *s1, char *s2);
+
+
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 int _putchar(char c);
+int _atoi(char *s);
 void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
+
+/**###### MEMORIE  MANGMENT ####*/
+
+void free_env(char **env);
+void *fill_an_array(void *a, int el, unsigned int len);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_calloc(unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_all(char **input, char *line);
+
+/**###### INPUT Function ######*/
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+/** ###### Command parser and extractor ###*/
+
+int path_cmd(char **line);
+char *_getenv(char *name);
+char **parse_cmd(char *cmd);
+int handle_builtin(char **cmd, int er);
+void read_file(char *filename, char **argv);
+char *build(char *token, char *value);
+int check_builtin(char **cmd);
+void creat_envi(char **envi);
+int check_cmd(char **tokens, char *line, int count, char **argv);
+void treat_file(char *line, int counter, FILE *fd, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
+
+/** ####BUL FUNC #####*/
+
+void hashtag_handle(char *buff);
+int history(char *input);
+int history_dis(char **cmd, int er);
+int dis_env(char **cmd, int er);
+int change_dir(char **cmd, int er);
+int display_help(char **cmd, int er);
+int echo_bul(char **cmd, int er);
+void  exit_bul(char **cmd, char *input, char **argv, int c);
+int print_echo(char **cmd);
+
+/** ####error handle and Printer ####*/
+void print_number(unsigned int n);
+void print_number_in(int n);
+void print_error(char *line, int c, char **argv);
+void _prerror(char **argv, int c, char **cmd);
+
+
+/**
+ * struct bulltin - contain bultin to handle and function to excute
+ * @command:pointer to char
+ * @fun:fun to excute when bultin true
+ */
+
+typedef struct  bulltin
+{
+	char *command;
+	int (*fun)(char **line, int er);
+} bul_t;
 
 #endif
